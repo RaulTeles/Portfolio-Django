b3f9a317eb11b7b655c36dbd0d7023cc00d39ca9
RaulTeles <telesraul@gmail.com>
2023-11-14 00:32:50 -0300

    local_settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,3 +129,8 @@ STATIC_ROOT = BASE_DIR / 'static_files'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from portfolio.local_settings import *
+except ImportError:
+    ...
